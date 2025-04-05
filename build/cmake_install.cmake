1268076c1648e2503d4fe6576b1f4cbb2ea06f4f
vsdmars <vsdmars@gmail.com>
2025-04-05 10:50:20 -0700

    update

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -34,7 +34,7 @@ endif()
 
 # Set path to fallback-tool for dependency-resolution.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/opt/homebrew/Cellar/llvm/19.1.7_1/bin/llvm-objdump")
+  set(CMAKE_OBJDUMP "/usr/bin/objdump")
 endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
