dc6d11a6c083d31415c9363c40992bb428a2eeb1
vsdmars <vsdmars@gmail.com>
2025-04-07 07:59:01 -0700

    update

--- cmakeFiles/compile.cmake
+++ cmakeFiles/compile.cmake
@@ -1,6 +1,7 @@
 # specify the C++ standard
-SET(CMAKE_CXX_STANDARD 20)
-SET(CMAKE_CXX_STANDARD_REQUIRED True)
+set(CMAKE_CXX_STANDARD 20)
+set(CMAKE_CXX_STANDARD_REQUIRED True)
+set(CMAKE_CXX_EXTENSIONS True)
 
 # debugging purpose, should be removed in production
 # https://stackoverflow.com/a/7032021
@@ -12,9 +13,11 @@ set(CMAKE_CXX_COMPILER "clang++")
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 
 # RPATH setup
-SET(CMAKE_SKIP_BUILD_RPATH  FALSE)
-SET(CMAKE_BUILD_WITH_INSTALL_RPATH FALSE)
-SET(CMAKE_INSTALL_RPATH_USE_LINK_PATH FALSE)
+set(CMAKE_SKIP_BUILD_RPATH  FALSE)
+set(CMAKE_BUILD_WITH_INSTALL_RPATH FALSE)
+set(CMAKE_INSTALL_RPATH_USE_LINK_PATH FALSE)
+
+add_compile_options(-Wall -Wextra -Werror=return-type -O2 -fexperimental-library)
 
 # compile with C++ 17 features
 # https://cmake.org/cmake/help/latest/manual/cmake-compile-features.7.html#requiring-language-standards
@@ -39,7 +42,9 @@ SET(CMAKE_INSTALL_RPATH_USE_LINK_PATH FALSE)
 # extra compile options
 # target_compile_options(${SCALE_LRUCACHE_BENCH} PRIVATE -O3 -Wall -Wconversion -Wdouble-promotion -Weffc++ -Wextra -Wfloat-equal -Wformat=2 -Wno-error=unused-variable -Wnull-dereference -Wold-style-cast -Wpedantic -Wshadow -Wuninitialized -Wunreachable-code -Wredundant-move -Wpessimizing-move -Wthread-safety)
 
-target_compile_options(
-    cppactor PRIVATE
-    $<$<COMPILE_LANG_AND_ID:CXX,Clang,GNU>:-Werror=return-type>
-)
+
+
+# target_compile_options(
+#     cppactor PRIVATE
+#     $<$<COMPILE_LANG_AND_ID:CXX,Clang>:-Werror=return-type -fexperimental-library>
+# )
--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,74 @@
+#include <coroutine>
 #include <iostream>
+#include <stdexcept>
+#include <thread>
 
+using namespace std;
 
-auto main() -> int {
-  std::cout << "Hi" << std::endl;
+auto switch_to_new_thread(std::jthread& out) {
+  struct awaitable {
+    std::jthread* p_out;
+
+    bool await_ready() { 
+      cout << "DBG-await_ready\n";
+      return false; 
+    }
+
+    void await_suspend(std::coroutine_handle<> h) {
+      cout << "DBG-await_suspend\n";
+      std::jthread& out = *p_out;
+      cout << "DBG-await_suspend old tid: " << out.get_id() << "\n";
+
+      if (out.joinable()) throw std::runtime_error("Output jthread parameter not empty");
+
+      out = std::jthread([h] {
+        cout << "DBG-inside new thread, about to call h.resume() " << " tid: " 
+          << std::this_thread::get_id() << "\n";
+        
+        h.resume(); }
+      );
+
+      // Potential undefined behavior: accessing potentially destroyed *this
+      // std::cout << "New thread ID: " << p_out->get_id() << '\n';
+      std::cout << "DBG-await_suspend New thread ID: " << out.get_id() << '\n';  // this is OK
+    }
+
+    void await_resume() {}
+  };
+
+  cout << "DBG-switch_to_new_thread before return awaitable\n";
+  return awaitable{&out};
+}
+
+struct task {
+  struct promise_type {
+    task get_return_object() { return {}; }
+    std::suspend_never initial_suspend() { return {}; }
+    std::suspend_never final_suspend() noexcept { return {}; }
+    void return_void() {}
+    void unhandled_exception() {}
+  };
+};
+
+task resuming_on_new_thread(std::jthread& out) {
+  std::cout << "Coroutine started on thread: " << 
+    std::this_thread::get_id() << " out jthread: " << out.get_id() << '\n';
+
+  cout << "DBG-1\n";
+
+  co_await switch_to_new_thread(out);
+  
+  cout << "DBG-2\n";
+  
+  // awaiter destroyed here
+  std::cout << "Coroutine resumed on thread: " << std::this_thread::get_id() << 
+    " out jthread: " << out.get_id() << '\n';
+}
+
+int main() {
+  std::jthread out;
+  resuming_on_new_thread(out);
+  
+  std::this_thread::sleep_for(std::chrono::seconds(5));
+  cout << "main thread: " << std::this_thread::get_id() << "\n";
 }
